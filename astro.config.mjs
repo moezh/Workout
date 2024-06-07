5f270dbade26072004af5d6dce73ae5326ed33de
Moez Hachicha <moez.hachicha@icloud.com>
2024-06-07 09:29:22 +0100

    0.1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://workout.com6.cc",
-  integrations: [sitemap({
-    lastmod: new Date()
-  }), tailwind(), react()]
+  integrations: [react(), tailwind()],
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/noop",
+    },
+  },
 });
